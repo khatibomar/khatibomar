f2fadbb508ff97f9cc205e9376af28a5ad55801e
OmarElKhatib <elkhatibomar@outlook.com>
2020-11-01 07:30:33 +0200

    Update update_readme.rb

--- scripts/rb/update_readme.rb
+++ scripts/rb/update_readme.rb
@@ -111,9 +111,6 @@ markdown = <<~EOF
 <a href="https://github.com/OmarElKhatibCS" target="_blank">
 <img src=https://img.shields.io/badge/github-%2324292e.svg?&style=for-the-badge&logo=github&logoColor=white alt=github style="margin-bottom: 5px;" />
 </a>
-<a href="https://twitter.com/omarkhwarzmi" target="_blank">
-<img src=https://img.shields.io/badge/twitter-%2300acee.svg?&style=for-the-badge&logo=twitter&logoColor=white alt=twitter style="margin-bottom: 5px;" />
-</a>
 <a href="https://dev.to/elkhatibomar" target="_blank">
 <img src=https://img.shields.io/badge/dev.to-%2308090A.svg?&style=for-the-badge&logo=dev.to&logoColor=white alt=devto style="margin-bottom: 5px;" />
 </a>
