84e82979b8526d20e40951b5869231c119b46d67
OmarKhatib <elkhatibomar@outlook.com>
2021-02-14 23:46:07 +0200

    Fix API

--- scripts/rb/update_readme.rb
+++ scripts/rb/update_readme.rb
@@ -18,7 +18,7 @@ posts = JSON.parse(response.body).map do |article|
   EOF
 end
 
-quote_response = Faraday.get "https://programming-quotes-api.herokuapp.com/quotes/random/lang/en/"
+quote_response = Faraday.get "http://quotes.stormconsultancy.co.uk/random.json"
 quote = JSON.parse(quote_response.body)
 
 # Generate your own layout and paste posts in it
@@ -130,7 +130,7 @@ My last publication:
 Quote of today :
     \n
   
-#{quote['en']}  - #{quote['author']}
+#{quote['quote']}  - #{quote['author']}
   
 ---
 <table><tbody><tr><td><a href="https://octo-ring.com/"><img src="https://octo-ring.com/static/img/widget/top.png" width="99%" alt="Octo Ring logo" align="top"></a><br><a href="https://octo-ring.com/p/OmarElKhatibCS/prev"><img src="https://octo-ring.com/static/img/widget/prev.png" width="33%" alt="previous" align="top" title="previous profile"></a><a href="https://octo-ring.com/p/OmarElKhatibCS/random"><img src="https://octo-ring.com/static/img/widget/random.png" width="33%" alt="random" align="top" title="random profile"></a><a href="https://octo-ring.com/p/OmarElKhatibCS/next"><img src="https://octo-ring.com/static/img/widget/next.png" width="33%" alt="next" align="top" title="next profile"></a><br><a href="https://octo-ring.com/"><img src="https://octo-ring.com/static/img/widget/bottom.png" width="99%" alt="check out other GitHub profiles in the Octo Ring" align="top"></a></td></tr></tbody></table>
